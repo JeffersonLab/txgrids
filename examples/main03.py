45cdd6430a699afe89590c2ad44911f39e917b24
nobuo <nobuosato@gmail.com>
2020-05-19 08:23:36 -0400

    update

---
+++ examples/main03.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+######################################
+# authors: N.Sato (nsato@jlab.org) 
+#
+# last update: 05-19-20
+#####################################
+import sys,os
+sys.path.append(os.path.dirname( os.path.dirname(os.path.abspath(__file__) ) ) )
+import numpy as np
+
+import lhapdf
+#--matplotlib
+import matplotlib
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
+matplotlib.rc('text',usetex=True)
+import pylab  as py
+
+Q2=90.0
+X1=10**np.linspace(-4,-1)
+X2=np.linspace(0.101,0.99)
+X=np.append(X1,X2)
+
+def get_stf(tabname,iset,iF2,iFL,iF3):
+    stf=lhapdf.mkPDF(tabname,iset)
+    F2=[stf.xfxQ2(iF2,x,Q2) for x in X]  
+    FL=[stf.xfxQ2(iFL,x,Q2) for x in X]
+    F3=[stf.xfxQ2(iF3,x,Q2) for x in X]
+    return F2,FL,F3
+
+ax=py.subplot(111)
+
+tabname='JAM4EIC'             
+iset,iF2,iFL,iF3=0,90001,90002,90003  
+F2,FL,F3 = get_stf(tabname,iset,iF2,iFL,iF3)
+ax.plot(X,F2,label='F2 JAM')
+ax.plot(X,FL,label='FL JAM')
+ax.plot(X,F2,label='F3 JAM')
+
+
+tabname='NNPDF31_nnlo_pch_as_0118_rs_0.5_SF'     
+iset,iF2,iFL,iF3=0,1001,1002,1003  
+F2,FL,F3 = get_stf(tabname,iset,iF2,iFL,iF3)
+ax.plot(X,F2,label='F2 NNPDF')
+ax.plot(X,FL,label='FL NNPDF')
+ax.plot(X,F3,label='F3 NNPDF')
+
+ax.legend()
+ax.set_ylim(0,1.7)
+ax.semilogx()
+ax.set_ylabel(r'$F_x(x,Q^2=100)$')
+ax.set_xlabel(r'$x$')
+
+py.tight_layout()
+py.savefig('test.pdf')
+
+
+
+
+
