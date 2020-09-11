372846b18ee443d737bbab6ab479c92220cef713
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2020-09-11 13:06:51 +0200

    new structure of stat-tests file, new updates

--- stat-tests/chi2-test_plot.py
+++ stat-tests/chi2-test_plot.py
@@ -30,75 +30,13 @@ from  matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text',usetex=True)
 
-def save_obj(obj, path ):
-    with open(path, 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
 def load_obj(path):
     with open(path, 'rb') as f:
         return pickle.load(f)
 
-def GetEvents(path,lum_arg,veto,sign,rep=0):
-    """
-    sign: --electron=1 positron=-1
-
-    """
-    Sample = {'min':{}, 'max':{}}
-
-    Sample['min']['tabname'] = min_SF
-    Sample['min']['iset'] = rep
-    Sample['min']['iF2'] = 908
-    Sample['min']['iFL'] = 909
-    Sample['min']['iF3'] = 910
-
-    Sample['max']['tabname'] = max_SF
-    Sample['max']['iset'] = rep
-    Sample['max']['iF2'] = 908
-    Sample['max']['iFL'] = 909
-    Sample['max']['iF3'] = 910
-
-    #common keys
-    for key in Sample.keys():
-        Sample[key]['wdir'] = path
-        Sample[key]['sign']      = sign
-        Sample[key]['rs'] = rs
-        Sample[key]['fname'] = 'mceg00'
-        Sample[key]['veto'] = veto
-        Sample[key]['fdata'] = path
-
-    #--generate events
-    #output keys: ['Y', 'X', 'Q2', 'W', 'rs']
-    print("--Getting Events--")
-    for key in Sample.keys():
-        np.random.seed(seeds[key])
-        mceg = MCEG(**(Sample[key]))
-        mceg.buil_mceg(neval=neval, nitn=nitn, min_Q=min_Q)
-
-        Sample[key]['tot_xsec'] = mceg.get_xsectot()
-        Sample[key]['tot_xsec'] *= units('fb')
-
-        Sample[key]['var_xsec'] = mceg.get_xsecvar()
-        Sample[key]['var_xsec'] = Sample[key]['var_xsec']**0.5*units('fb')
-
-        Sample[key]['quality_xsec'] = mceg.get_quality()
-
-        Nevents = int(lum_arg*Sample[key]['tot_xsec'])
-        
-        Sample[key]['Nevents'] = Nevents
-        
-        Sample[key].update(mceg.gen_events(Nevents))
-
-    keys_of_interest = ['W','X','Q2']
-
-    for key1 in Sample.keys():
-        for key2 in keys_of_interest:
-            Sample[key1][key2]=np.array(Sample[key1][key2])
-
-    return Sample
-
-def GetBins():
+def GetBins(pwd=""):
     Bins = {}
-    kin0, kin = gen_grid("../expdata/")
+    kin0, kin = gen_grid(pwd+"../expdata/")
     Bins['x'] = sorted(set(np.array(kin)[:, 0]))
     Bins['Q2'] = sorted(set(np.array(kin)[:, 1]))
 
@@ -129,151 +67,6 @@ def GetBins():
         Bins['x'] = Xbins_smax
     """
     return Bins
-
-def GetCrossSections(Bins,sys_path="../expdata/data/xQ2binTable-xiaoxuan-060220+syst.npy"):
-
-    #--- systematics uncertainties
-    sys_bins = np.load(sys_path)
-
-    #initialization
-    hist_CrossSection={}
-    hist_CrossSection['non_empty'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']) > 1, 'max': np.zeros(Bins['Nx']*Bins['NQ2']) > 1}
-    hist_CrossSection['weights'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-    hist_CrossSection['xsecs'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-    hist_CrossSection['Nevents'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-    hist_CrossSection['statunc'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-    hist_CrossSection['MCunc'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-    hist_CrossSection['sysunc'] = {'min': np.zeros(Bins['Nx']*Bins['NQ2']), 'max': np.zeros(Bins['Nx']*Bins['NQ2'])}
-
-    #to get:
-    #Sample[key]['Nevents']: hist_CrossSection['weights']['min']*Sample['max']['tot_xsec']*lum (per bin)
-    #tot xsec: Sample['min']['tot_xsec'] (in fb)
-    #normalized xsec: hist_CrossSection['weights']['min']
-
-    print("--Filling hist_CrossSection--")
-    for key in hist_CrossSection['weights'].keys():
-        for iQ2 in range(0, Bins['NQ2']):
-            for iX in range(0, Bins['Nx']):
-
-                x_mask  = np.where((Sample[key]['X'] > Bins['x'][iX]) & (Sample[key]['X'] < Bins['x'][iX+1]),True,False)
-                Q2_mask = np.where((Sample[key]['Q2'] > Bins['Q2'][iQ2]) & (Sample[key]['Q2'] < Bins['Q2'][iQ2+1]),True,False)
-                mask=x_mask*Q2_mask
-                weight = np.sum(Sample[key]['W'][mask])
-                xsec = weight*Sample[key]['tot_xsec'] # [fb]
-                N = float(int(xsec*lum_arg))
-                hist_CrossSection['weights'][key][iQ2*Bins['Nx']+iX] = weight
-                hist_CrossSection['xsecs'][key][iQ2*Bins['Nx']+iX] = xsec
-                hist_CrossSection['Nevents'][key][iQ2*Bins['Nx']+iX] = N
-                if N==0:
-                    hist_CrossSection['non_empty'][key][iQ2*Bins['Nx']+iX] = False
-                    hist_CrossSection['statunc'][key][iQ2*Bins['Nx']+iX] = 0.
-                    hist_CrossSection['MCunc'][key][iQ2*Bins['Nx']+iX] = 0.
-                    hist_CrossSection['sysunc'][key][iQ2*Bins['Nx']+iX] = 0.
-                else:
-                    hist_CrossSection['non_empty'][key][iQ2*Bins['Nx']+iX] = True
-                    hist_CrossSection['statunc'][key][iQ2*Bins['Nx']+iX] = np.sqrt(N)/lum_arg
-                    hist_CrossSection['MCunc'][key][iQ2*Bins['Nx']+iX] = weight*Sample[key]['var_xsec']
-
-                    relative_sys = np.where(
-                        (sys_bins[:, 0] == Bins['x_cv'][iX]) & (sys_bins[:, 1] == Bins['Q2_cv'][iQ2]))
-                    if np.any(relative_sys): #if bin has sys uncertainty
-                        relative_sys = sys_bins[relative_sys][0, 2]
-                    else: #if bin doesn't have sys uncertainty
-                        relative_sys = 0
-
-                    hist_CrossSection['sysunc'][key][iQ2*Bins['Nx']+iX] = xsec*relative_sys
-
-    hist_CrossSection['non_empty']['total'] = hist_CrossSection['non_empty']['min']+hist_CrossSection['non_empty']['max']
-
-    hist_CrossSection['non_empty']['map'] = {}
-    count = 0
-    for iQ2 in range(0, Bins['NQ2']):
-        for iX in range(0, Bins['Nx']):
-            if hist_CrossSection['non_empty']['total'][iQ2*Bins['Nx']+iX]:
-                hist_CrossSection['non_empty']['map'][iQ2 *
-                                                      Bins['Nx']+iX] = count
-                count += 1
-
-    Bins['total'] = np.count_nonzero(hist_CrossSection['non_empty']['total'])
-
-    return hist_CrossSection
-
-def GetCovMat(Bins, hist_CrossSection):
-
-    CovMat = {'xsecs': {'min': np.zeros((Bins['Nx']*Bins['NQ2'], Bins['Nx']*Bins['NQ2'])), 'max': np.zeros(
-        (Bins['Nx']*Bins['NQ2'], Bins['Nx']*Bins['NQ2'])), 'total': np.zeros((Bins['Nx']*Bins['NQ2'], Bins['Nx']*Bins['NQ2']))}}
-        
-    print("--Building covariance matrix--")
-    for key in hist_CrossSection['weights'].keys():
-        for iQ2 in range(0, Bins['NQ2']):
-            for iX in range(0, Bins['Nx']):
-
-                if hist_CrossSection['Nevents'][key][iQ2*Bins['Nx']+iX] == 0:
-                    CovMat['xsecs'][key][iQ2*Bins['Nx']+iX][iQ2*Bins['Nx']+iX] = 0.
-                else:
-                    CovMat['xsecs'][key][iQ2*Bins['Nx']+iX][iQ2*Bins['Nx']+iX] = (hist_CrossSection['statunc'][key][iQ2*Bins['Nx']+iX])**2 + (
-                        hist_CrossSection['MCunc'][key][iQ2*Bins['Nx']+iX])**2 + (hist_CrossSection['sysunc'][key][iQ2*Bins['Nx']+iX])**2
-                    #covmat_weights[key][iQ2*Bins['Nx']+iX][iQ2*Bins['Nx']+iX] = (np.sqrt(N)/(Sample[key]['tot_xsec']*lum))**2
-
-    CovMat['xsecs']['total'] = np.matrix(np.zeros((Bins['total'], Bins['total'])))
-
-    for key in hist_CrossSection['xsecs'].keys():
-        CovMat['xsecs'][key] = CovMat['xsecs'][key][hist_CrossSection['non_empty']['total'], :]
-        CovMat['xsecs'][key] = CovMat['xsecs'][key][:, hist_CrossSection['non_empty']['total']]
-
-        CovMat['xsecs'][key] = np.matrix(CovMat['xsecs'][key])
-        CovMat['xsecs']['total'] += CovMat['xsecs'][key]
-    
-    return CovMat
-
-def GetAnalysis(Bins, hist_CrossSection):
-    hist_Analysis={}
-    hist_Analysis['chi2s'] = np.zeros(Bins['Nx']*Bins['NQ2'])
-    hist_Analysis['zscores'] = np.zeros(Bins['Nx']*Bins['NQ2'])
-    hist_Analysis['x'] = np.zeros(Bins['Nx']*Bins['NQ2'])
-    hist_Analysis['Q2'] = np.zeros(Bins['Nx']*Bins['NQ2'])
-
-    hist_Analysis['xsecs'] = {}
-    for key in hist_CrossSection['xsecs'].keys():
-        hist_Analysis['xsecs'][key] = np.matrix(hist_CrossSection['xsecs'][key][hist_CrossSection['non_empty']['total']])
-
-    print("--Filling hist_Analysis--")
-    for iQ2 in range(0, Bins['NQ2']):
-        for iX in range(0, Bins['Nx']):
-            if hist_CrossSection['non_empty']['total'][iQ2*Bins['Nx']+iX]:
-                ind = hist_CrossSection['non_empty']['map'][iQ2*Bins['Nx']+iX]
-
-                max_d = hist_Analysis['xsecs']['max'][0,ind]
-                min_d = hist_Analysis['xsecs']['min'][0,ind]
-
-                hist_Analysis['chi2s'][iQ2*Bins['Nx']+iX] = (min_d-max_d)*inv_covmat_xsecs[ind,ind]*(min_d-max_d)
-                hist_Analysis['zscores'][iQ2*Bins['Nx']+iX] = np.sqrt(hist_Analysis['chi2s'][iQ2*Bins['Nx']+iX])
-                if hist_Analysis['zscores'][iQ2*Bins['Nx']+iX]<1:
-                    hist_Analysis['zscores'][iQ2*Bins['Nx']+iX]=-1
-                else:
-                    hist_Analysis['zscores'][iQ2*Bins['Nx']+iX] = int(hist_Analysis['zscores'][iQ2*Bins['Nx']+iX])
-
-            hist_Analysis['x'][iQ2*Bins['Nx']+iX] = Bins['x_cv'][iX]
-            hist_Analysis['Q2'][iQ2*Bins['Nx']+iX] = Bins['Q2_cv'][iQ2]
-
-            ##hist_Analysis['x'][iQ2*Bins['Nx']+iX] = Bins['x'][iX]+(Bins['x'][iX+1]-Bins['x'][iX])/2.
-            ##hist_Analysis['Q2'][iQ2*Bins['Nx']+iX] = Bins['Q2'][iQ2]+(Bins['Q2'][iQ2+1]-Bins['Q2'][iQ2])/2.
-
-    hist_Analysis['tot_chi2'] = (hist_Analysis['xsecs']['min']-hist_Analysis['xsecs']['max'])*inv_covmat_xsecs*(hist_Analysis['xsecs']['min']-hist_Analysis['xsecs']['max']).T
-    hist_Analysis['tot_chi2'] /= Bins['total']
-
-    hist_Analysis['zscores'][hist_Analysis['zscores'] >= 5] = 5
-
-    #replace all 0 bins with nan for the hist to be properly colored:
-    for key in hist_CrossSection['xsecs'].keys():
-        hist_CrossSection['weights'][key][hist_CrossSection['weights'][key]==0] = np.nan
-        hist_CrossSection['xsecs'][key][hist_CrossSection['xsecs'][key]==0] = np.nan
-        hist_Analysis['chi2s'][hist_Analysis['chi2s']==0] = np.nan
-        hist_Analysis['zscores'][hist_Analysis['zscores'] == 0] = np.nan
-
-    hist_Analysis['zscores'][hist_Analysis['zscores'] == -1] = 0
-
-    return hist_Analysis
 
 def units(units):
     one=0.3893793721 #--GeV2 mbarn
@@ -453,7 +246,7 @@ if __name__ == "__main__":
 
     #--- Getting arguments from user
     if len(sys.argv) < 2:
-        print "usage: ./stat-tests.py <Luminosity (fb^-1)> <pdf_replica>"
+        print "usage: ./chi2-test_plot.py <Luminosity (fb^-1)> <pdf_replica=-1 for total analysis>"
         exit(1)
 
     lum_arg = float(sys.argv[1])
@@ -469,13 +262,20 @@ if __name__ == "__main__":
     sub_wdir = wdir+"lum"+str(lum_arg)+"/"
     if not os.path.isdir(sub_wdir): os.system("mkdir "+sub_wdir)
 
-    sub_sub_wdir = sub_wdir+"pdfrep" + str(rep)+"/"
-    if not os.path.isdir(sub_sub_wdir): os.system("mkdir "+sub_sub_wdir)
+    sub_sub_wdir = ""
+    if rep != -1:
+        sub_wdir+"pdfrep" + str(rep)+"/"
+        if not os.path.isdir(sub_sub_wdir): os.system("mkdir "+sub_sub_wdir)
 
     Sample_path = sub_sub_wdir+"Sample.p"
     hist_CrossSection_path = sub_sub_wdir+"hist_CrossSection.p"
     CovMat_path = sub_sub_wdir+"CovMat.p"
     hist_Analysis_path = sub_sub_wdir+"hist_Analysis.p"
+
+    Sample={}
+    hist_CrossSection={}
+    CovMat={}
+    hist_Analysis={}
     #----
 
     #--physical params and cuts
@@ -499,7 +299,7 @@ if __name__ == "__main__":
     #-----------------------------------------------
 
     #--- acceptance/purity based binning
-    Bins=GetBins()
+    Bins=GetBins(pwd)
 
     #--- kinematic cuts
     def veto00(x,y,Q2,W2):
@@ -509,22 +309,22 @@ if __name__ == "__main__":
         
     #--Getting events
     if not os.path.isfile(Sample_path):
-        Sample = GetEvents(sub_sub_wdir, lum_arg, veto00, sign=1, rep=rep)
-        save_obj(Sample,Sample_path)
+        print(Sample_path+" doesn't exist, please run chi2-test_generate.py first.")
+        exit(1)
     else:
-        load_obj(Sample_path)
+        Sample = load_obj(Sample_path)
 
     #--Getting CrossSections
     if not os.path.isfile(hist_CrossSection_path):
-        hist_CrossSection = GetCrossSections(Bins, sys_path=pwd+"../expdata/data/xQ2binTable-xiaoxuan-060220+syst.npy")
-        save_obj(hist_CrossSection,hist_CrossSection_path)
+        print(hist_CrossSection_path+" doesn't exist, please run chi2-test_generate.py first.")
+        exit(1)
     else:
         load_obj(hist_CrossSection_path)
 
     #--Getting CovMat
     if not os.path.isfile(CovMat_path):
-        CovMat = GetCovMat(Bins, hist_CrossSection)
-        save_obj(CovMat,CovMat_path)
+        print(CovMat_path+" doesn't exist, please run chi2-test_generate.py first.")
+        exit(1)
     else:
         load_obj(CovMat_path)
 
@@ -537,4 +337,44 @@ if __name__ == "__main__":
     else:
         load_obj(hist_Analysis_path)
 
-    print("All data has been generated here: "+sub_sub_wdir)
+    #--Plotting-----------------------------------------------
+    #---------------------------------------------------------
+    print("\n--Plotting--")
+
+    #--General settings
+    gs,fig=plt_SetGridSpec((2, 4))
+
+    #---- Chi2 hist + sys unc + stat unc 
+    title_pos=(0.5,1.04)
+
+    plt_chi2s(0, title_pos, Bins, hist_Analysis)
+
+    plt_xsecs([0,1],'max', title_pos, Bins, hist_Analysis, Sample)
+    plt_xsecs([1,1],'min', title_pos, Bins, hist_Analysis, Sample)
+
+    plt_unc([0,2],'sysunc','max', title_pos, Bins, hist_Analysis, hist_CrossSection)
+    plt_unc([1,2],'sysunc','min', title_pos, Bins, hist_Analysis, hist_CrossSection)
+
+    plt_unc([0,3],'statunc','max', title_pos, Bins, hist_Analysis, hist_CrossSection)
+    plt_unc([1,3],'statunc','min', title_pos, Bins, hist_Analysis, hist_CrossSection)
+
+    plt.savefig("chi2-overview-"+str(lum_arg)+"fb-1_pdfrep"+str(rep)+".pdf")
+    plt.cla()
+    plt.clf()
+
+
+    #---- Chi2 hist alone with values written per bin
+    print " "
+    plt_Detailedchi2s(Bins, hist_Analysis)
+
+    plt.savefig("chi2-detailed-"+str(lum_arg)+"fb-1_pdfrep"+str(rep)+".pdf")
+    plt.cla()
+    plt.clf()
+
+    #---- Zscore hist alone with discrete colors
+    print " "
+    plt_DetailedZscore(Bins,hist_Analysis)
+
+    plt.savefig("Zscore-detailed-"+str(lum_arg)+"fb-1_pdfrep"+str(rep)+".pdf")
+    plt.cla()
+    plt.clf()
